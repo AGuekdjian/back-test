caf3e0956e69892ee8a79643f0d9d3ba97b83494
Anthony Guekdjian <anthonyguekdjian@gmail.com>
2024-03-21 14:02:57 -0300

    modify script

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker compose down
-docker image rm prob-back
-docker image build -t prob-back .
-docker compose up -d
+sudo docker compose down
+sudo docker image rm prob-back
+sudo docker image build -t prob-back .
+sudo docker compose up -d
